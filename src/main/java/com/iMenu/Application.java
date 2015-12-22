907e16436c2a7a1b01e4c9d0fe45e0459a75db1e
armansabaa <abdelrahman.elsabaa@gmail.com>
2015-12-23 01:32:42 +0200

    Fixing the last commit

--- src/main/java/com/iMenu/Application.java
+++ src/main/java/com/iMenu/Application.java
@@ -2,7 +2,6 @@ package com.iMenu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.aws.context.config.annotation.EnableContextCredentials;
 import org.springframework.cloud.aws.context.config.annotation.EnableContextRegion;
@@ -15,11 +14,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableContextCredentials(accessKey="AKIAID7TMHP76YHKFJGA", secretKey="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")
 @EnableContextRegion(region = "eu-central-1")
 public class Application extends SpringBootServletInitializer {
-=======
-
-@SpringBootApplication
-public class Application {
->>>>>>> d0d0de717a7d0c2a46cf1393d0aeb4ead4cc7093
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
